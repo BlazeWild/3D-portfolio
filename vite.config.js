fbe6cfaa9b2846f19526531e52426cc0930d0e71
Ashok Bk <ashokbk215@gmail.com>
2024-09-21 14:12:30 +0545

    added

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  // build: {
-  //   chunkSizeWarningLimit: 1000, // Adjust the chunk size limit if needed
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks(id) {
-  //         if (id.includes('node_modules')) {
-  //           return id.split('node_modules/')[1].split('/')[0].toString(); // Splits based on module name
-  //         }
-  //       },
-  //     },
-  //   },
-  // },
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust the chunk size limit if needed
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.split('node_modules/')[1].split('/')[0].toString(); // Splits based on module name
+          }
+        },
+      },
+    },
+  },
 });
