04e669f6f95844de63c4f070925d2c81d5b8ea94
Ashok Bk <ashokbk215@gmail.com>
2024-09-21 10:18:26 +0545

    added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {About, Contact, Experience, Feedbacks, Hero,
 const App=() =>{
   return (
 <BrowserRouter>
-<div className="relative z-0 bg-primary">
+<div className="relative z-0 bg-primary overflow-x-hidden">
   <div className="bg-hero-pattern bg-cover">
     <Navbar />
     <Hero />
